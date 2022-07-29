00e8096b07460d8b953730d2c864f3ad529b4bc0
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-07-29 12:43:44 -0500

    My Car Gas mileage method

--- launchschool/mycar.rb
+++ launchschool/mycar.rb
@@ -1,14 +1,20 @@
 =begin
-Create a class called MyCar. When you initialize a new instance or object of the class, allow the user to define some 
+
+        Classes and Objects I
+1. Create a class called MyCar. When you initialize a new instance or object of the class, allow the user to define some 
 instance variables that tell us the year, color, and model of the car. Create an instance variable that is set to 0 
 during instantiation of the object to track the current speed of the car as well. Create instance methods that allow 
 the car to speed up, brake, and shut the car off.
 
-Add an accessor method to your MyCar class to change and view the color of your car. Then add an accessor method that
+2. Add an accessor method to your MyCar class to change and view the color of your car. Then add an accessor method that
 allows you to view, but not modify, the year of your car.
 
-You want to create a nice interface that allows you to accurately describe the action you want your program to perform. 
+3. You want to create a nice interface that allows you to accurately describe the action you want your program to perform. 
 Create a method called spray_paint that can be called on an object and will modify the color of the car.
+
+
+        Classes and Objects II
+1. Add a class method to your MyCar class that calculates the gas mileage of any car.
 =end
 
 
@@ -43,6 +49,10 @@ class MyCar
     self.color = color
   end
 
+  def self.gas_mileage(gallons, miles)
+    puts "#{miles / gallons} miles per gallon of gas"
+  end
+
 end
 
 lumina = MyCar.new(1997, 'white', 'chevy lumina')
@@ -61,3 +71,4 @@ puts lumina.color
 puts lumina.year
 lumina.spray_paint('red')
 puts lumina.color
+MyCar.gas_mileage(13, 351)
